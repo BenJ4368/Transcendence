d16887b33e641c426f76c60e4eea3f82b11c0374
benja <benja@Transcendence.myguest.virtualbox.org>
2024-09-18 13:37:06 +0200

    dev script

--- django/project_transcendence/settings.py
+++ django/project_transcendence/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'project_transcendence.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'app_transcendence', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
